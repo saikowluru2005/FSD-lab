acbaa7c8adb24530aa239d771b33b792fa26b7d6
saikowluru2005 <saikowluru3@gmail.com>
2025-01-07 21:29:34 +0530

    q10

---
+++ q10.js
@@ -0,0 +1,22 @@
+class BankAccount {
+    constructor(accountHolder, balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+    }
+
+    withdraw(amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+        } else {
+            console.log("Insufficient balance");
+        }
+    }
+}
+
+function transfer(account, amount) {
+    account.withdraw(amount);
+}
